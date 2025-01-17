338991e9b725c072fd4b4792ce3d3cd112c10dc0
biroman <eivind-buodd@hotmail.com>
2025-01-17 17:05:16 +0100

    tools margin fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -35,11 +35,11 @@ const Projects: FC = () => {
             />
           </div>
           <div className="flex flex-col md:ml-8 p-4 md:pl-8 justify-center w-full">
-            <div className="flex flex-col md:flex-row items-start md:items-center">
+            <div className="flex flex-row md:flex-row items-start md:items-center">
               <h1 className="font-bold text-2xl md:text-3xl">
                 {t(`projects.${projectId}.name`)}
               </h1>
-              <div className="flex mt-2 md:mt-0 md:flex-col">
+              <div className="flex mt-1 ml-2 md:mt-0 md:ml-0">
                 {Array.isArray(
                   t(`projects.${projectId}.tools`, { returnObjects: true })
                 )
